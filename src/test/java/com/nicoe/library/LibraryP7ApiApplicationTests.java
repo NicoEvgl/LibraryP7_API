de6aec1b8c2cd4d01f28275285efef8039b6bbbc
Nico Evgl <nico.evangelista33@gmail.com>
2020-10-30 16:46:15 +0100

    first commit - build Maven project / pom.xml

---
+++ src/test/java/com/nicoe/library/LibraryP7ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoe.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryP7ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
