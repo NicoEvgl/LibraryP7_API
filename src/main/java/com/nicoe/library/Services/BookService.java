af16716719d39dc4f15daf1eacdf7e848fa27a54
Nico Evgl <nico.evangelista33@gmail.com>
2021-02-05 10:48:14 +0100

    edit BookService + BookServiceImpl to add reservation function

--- src/main/java/com/nicoe/library/Services/BookService.java
+++ src/main/java/com/nicoe/library/Services/BookService.java
@@ -4,6 +4,7 @@ import com.nicoe.library.model.entities.Book;
 import com.nicoe.library.model.entities.Copy;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,4 +12,7 @@ public interface BookService {
     List<Book> bookList();
     Integer findAvailableBook(Integer id);
     List<Copy> loanInLate();
+    Integer nbAllowedReservations(Integer bookId);
+    Date returnDate(Integer bookId);
+    Integer findRank(Integer bookId, Integer userId);
 }
--- src/main/java/com/nicoe/library/Services/impl/BookServiceImpl.java
+++ src/main/java/com/nicoe/library/Services/impl/BookServiceImpl.java
@@ -3,8 +3,10 @@ package com.nicoe.library.Services.impl;
 import com.nicoe.library.Services.BookService;
 import com.nicoe.library.model.dao.BookDao;
 import com.nicoe.library.model.dao.CopyDao;
+import com.nicoe.library.model.dao.ReservationDao;
 import com.nicoe.library.model.entities.Book;
 import com.nicoe.library.model.entities.Copy;
+import com.nicoe.library.model.entities.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,8 @@ public class BookServiceImpl implements BookService {
     BookDao bookDao;
     @Autowired
     CopyDao copyDao;
+    @Autowired
+    ReservationDao reservationDao;
 
     @Override
     public List<Book> bookList() {
@@ -36,5 +40,33 @@ public class BookServiceImpl implements BookService {
         Date date = new Date();
         List<Copy> copies = copyDao.findAllByLoanEndDateBefore(date);
         return copies;
+    }
+
+    @Override
+    public Integer nbAllowedReservations(Integer bookId) {
+        return bookDao.findAllByBookId(bookId).getCopies().size() * 2;
+    }
+
+    @Override
+    public Date returnDate(Integer bookId) {
+        List<Copy> copies = copyDao.findAllByBook_BookIdOrderByLoanEndDate(bookId);
+        Date endDate = null;
+        if (!copies.isEmpty()) {
+            endDate = copies.get(0).getLoanEndDate();
+        }
+        return endDate;
+    }
+
+    @Override
+    public Integer findRank(Integer bookId, Integer userId) {
+        List<Reservation> reservations = reservationDao.findAllByBook_BookIdOrderByReservationId(bookId);
+        Integer rank = 1;
+        for (int i = 0; i < reservations.size(); i++) {
+            if (reservations.get(i).getUser().getUserId().equals(userId)) {
+                rank = rank + 1;
+            }
+            break;
+        }
+        return rank;
     }
 }
